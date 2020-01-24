3dfca4730d443dbb88c429f4ed4426d3c0bf66dc
masud001 <madhnagar@gmail.com>
2020-01-25 01:58:49 +0600

    updated an array

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-let selected;
+// Learning JavaScript Array
+
+let selectedColors = ["red", "green", "yellow", 12, 3, 22];
+// update an array
+selectedColors[2] = "Masud";
+console.log(selectedColors);
