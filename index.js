12f5430e0a269197bd540b590585992a8affa6f4
masud001 <madhnagar@gmail.com>
2020-01-25 03:12:10 +0600

    javascript basic function, 2type of function we discussed here 1. performing task, 2. calculation

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-// Learning JavaScript Array
-// array itself is an Object with key value paries....
-// Array is a data structure, that we use to represent list of items
-let selectedColors = [
-	"red",
-	"green",
-	"yellow",
-	12,
-	3,
-	22,
-	true,
-	false,
-	undefined,
-	null
-];
-// update an array
-selectedColors[2] = "Masud";
+// Learning JavaScript Functions.
+// 'Function' is one of the funddamental biulding blocks of javascript.
 
-console.log(selectedColors);
+// this is the function which is perfforming a task
+function greet(firstName, lastName) {
+	console.log("Hello " + firstName + " " + lastName);
+}
+greet("Masudur", "Rahman");
+
+// V.V.I.N:
+// in this example, the 'statement' we declare console.log("hello world"),
+// So it should be terminated by semicolon [;]
+
+// but when we declaring a function we don't need to add semicolon[;] at the end of function.
+// because we are not declaring a variable let varName = "value"; . this is a function declaration.
+// when i we call this function, this is a 'statement' so we need semicolon
+
+// variable firstName and lastName are only meaningfull inside greet() function
+// in this function, greet(firstName, lastName) here firstName, lastName is called parameter of this function,
+// these parameter is not accessable from out side of this functions.
+// when we pass variable  with function statement greet("Masudur", "Rahman"),
+// here Masudur and Rahman is arguments of this functions
+
+// functions performing a calculations
+
+function squire(number) {
+	return number * number;
+}
+
+let number = squire(2);
+console.log(number);
+// we can store this 'return value' in a variable here 'number' is a variable.
+// we can use this value to initialize this in a variable "number"
+console.log(squire(4));
+// or we can declare this functon directly inside console
