b76a03bfe236ff6d6fa23e1507f9b0661e4e663c
masud001 <madhnagar@gmail.com>
2020-01-25 02:21:47 +0600

    finish array for now... Array is a data structure that we use to re present list of items

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Learning JavaScript Array
-
-let selectedColors = ["red", "green", "yellow", 12, 3, 22];
+// array itself is an Object with key value paries....
+// Array is a data structure, that we use to represent list of items
+let selectedColors = [
+	"red",
+	"green",
+	"yellow",
+	12,
+	3,
+	22,
+	true,
+	false,
+	undefined,
+	null
+];
 // update an array
 selectedColors[2] = "Masud";
+
 console.log(selectedColors);
