72bbf31f51a2573f68f6f7366420216ae3bb195b
masud001 <madhnagar@gmail.com>
2020-01-25 00:32:15 +0600

    start learning array

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-let person = {
-	name: "masud",
-	age: 23
-};
-
-// 2 ways to declare a object in javaScript
-// -------------------------------------------
-
-//1. javascript object declatetion with DOT notation
-person.name = "Masudur Rahman";
-console.log(person.name);
-
-//2. javaScript Bracket Notetion
-let select = "age";
-person[select] = "Muhammad Ali";
-
-console.log(person.age);
+let selected;
