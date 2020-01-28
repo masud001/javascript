c6b372e80ce516377ef769a336f26b6c9a583bc9
masud001 <madhnagar@gmail.com>
2020-01-28 16:38:49 +0600

    switch conditions statements for javascript done!

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ console.log(typeof hour);
 if (hour >= 6 && hour < 12) console.log("Good Morning!");
 else if (hour >= 12 && hour < 18) console.log("Good Afternoon!");
 else console.log("Good Evening");
+
+// switch conditions start
+
+let role = "Modarator";
+
+switch (role) {
+	case "User":
+		console.log(`Welcome ${role}`);
+		break;
+	case "Guest":
+		console.log(`Welcome ${role}`);
+		break;
+	case "Modarator":
+		console.log(`Welcome ${role}`);
+		break;
+	default:
+		console.log(`Default user : ${role}`);
+}
