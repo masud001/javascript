5e579a4e0edf5233ac788e19851e290ee885be6c
masud001 <madhnagar@gmail.com>
2020-02-06 09:33:10 +0600

    initiate factory functions

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // object-oriented programming (OOP)
 
 // circle object
-const circle = {
-	radius: 1,
-	locations: {
-		x: 1,
-		y: 2
-	},
-	isVisible: true,
-	draw: function() {
-		console.log("Draw function inside circle...");
-	}
-};
 
-circle.draw();
+// circle.draw();
+
+// factory function
+
+function createCircle(radius) {
+	return {
+		radius,
+		locations: {
+			x: 1,
+			y: 2
+		},
+		isVisible: true,
+		draw(a, b) {
+			console.log("Draw function inside circle...", a, b);
+		}
+	};
+}
+const circle1 = createCircle(2);
+console.log(circle1.draw("Masudur", " Rahman"));
