897cfae0ee2db0e81fb1ab41278af96f7eeed270
masud001 <madhnagar@gmail.com>
2020-02-06 09:07:04 +0600

    declare OOP in javascript

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// object-oriented programming (OOP)
 
+// circle object
+const circle = {
+	radius: 1,
+	locations: {
+		x: 1,
+		y: 2
+	},
+	isVisible: true,
+	draw: function() {
+		console.log("Draw function inside circle...");
+	}
+};
+
+circle.draw();
