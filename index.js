2dcba68e146c2d1c40ff46d055a17be3e403c704
masud001 <madhnagar@gmail.com>
2020-02-06 09:54:01 +0600

    initiate Constructor functions to create new object of circle in javascript

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ function createCircle(radius) {
 }
 const circle1 = createCircle(2);
 console.log(circle1.draw("Masudur", " Rahman"));
+
+// Constructor functions
+// Naming conventions in javascript are 2 types:
+// 1.CamelCase Notations... oneTwoThree.
+// 2.Pascel Notations...OneTwoThree.
+
+// Constructor uses Pascel Notations
+function CircleConstructor(radius) {
+	this.radius = radius;
+	this.draw = function() {
+		console.log("Constructor function draw method");
+	};
+}
+
+const circle = new CircleConstructor(10);
+console.log(circle.radius);
+console.log(circle.draw());
