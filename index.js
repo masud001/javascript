efe94c283deae4916d49e17d7ea7d9576048babf
masud001 <madhnagar@gmail.com>
2020-02-07 23:30:02 +0600

    adding elements in array have to method like push, unshift, splice, accordingly END,BEGINNING,AND MIDDLE of array

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ let selectedColors = [
 selectedColors[2] = "Masud";
 
 console.log(selectedColors);
+
+// adding an array
+
+const numbers = [3, 4];
+// add new elements in array
+// END
+numbers.push(5, 6);
+// BEGINNING
+numbers.unshift(1, 2);
+// MIDDLE
+numbers.splice(2, 0, "M", "A", "S", "U", "D");
+console.log(numbers);
